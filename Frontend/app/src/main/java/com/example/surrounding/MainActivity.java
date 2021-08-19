7b56068794311c14c86dc766d6e8b600d72261c4
kimnajin <3190021@gmail.com>
2021-08-19 21:51:33 +0900

    add loginActivity.java edit MainActivity.java -> for show loading window add activity_login.xml edit activity_main.xml -> for show loading window

--- Frontend/app/src/main/java/com/example/surrounding/MainActivity.java
+++ Frontend/app/src/main/java/com/example/surrounding/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.surrounding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+//import com.example.surrounding.ui.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
     }
 }
