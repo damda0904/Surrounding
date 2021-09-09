5886b1905bc506767244c22736daccd95a659f0c
nueeaz <j936596@sookmyung.ac.kr>
2021-09-09 15:11:50 +0900

    add several pages

---
+++ Frontend/app/src/main/java/com/example/surrounding/Infra_Form_Activity.java
@@ -0,0 +1,14 @@
+package com.example.surrounding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Infra_Form_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infra_form);
+    }
+}
